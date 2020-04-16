a26ce81b3d8fc4b062cbda181b0db3fd8d9fd9cc
RebornInferno <antonpirogov@mail.ru>
2020-04-16 03:51:12 +0300

    Experimenting with the architecture

---
+++ Assets/Weapon Structure Test.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Weapon
+{
+    public enum WeaponType
+    {
+        RangedWeapon,
+        MeleeWeapon
+    }
+    public enum WeaponState
+    {
+        NoBullets,
+        OnAttackCooldown,
+        WeaponJammed,
+    }
+
+    public interface IWeapon
+    {
+        int BaseAttackDamage { get; set; }
+        float WeaponKnockback { get; set; }
+        WeaponState WeaponState { get; set; }
+
+        WeaponType WeaponType { get; set; }
+
+        void DoAttack();
+        void Initialize();
+
+    }
+
+    public interface IReloadable : IWeaponComponent
+    {
+
+    }
+
+    public class TestWeapon : IWeapon, IReloadable, IMelee
+    {
+        public int BaseAttackDamage { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public float WeaponKnockback { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public WeaponState WeaponState { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public WeaponType WeaponType { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public void DoAttack()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface IMelee : IWeaponComponent
+    {
+
+    }
+    public interface IWeaponComponent
+    {
+
+    }
+    public abstract class RangedWeapon : IWeapon
+    {
+        public abstract int BaseAttackDamage { get; set; }
+        public float WeaponKnockback { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public WeaponState WeaponState { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public WeaponType WeaponType { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public int fireRate;
+        public int attackRate;
+
+        public void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void DoAttack()
+        {
+            Debug.Log("Pew Pew");
+        }
+
+        public virtual void Reload()
+        {
+            Debug.Log("Pew Pew");
+        }
+
+
+    }
+
+    public abstract class MeleeWeapon : IWeapon
+    {
+        public abstract int BaseAttackDamage { get; set; }
+        public float WeaponKnockback { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public WeaponState WeaponState { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public WeaponType WeaponType { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public float attackRange;
+        public int attackRate;
+
+        public void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void DoAttack()
+        {
+            Debug.Log("Slash Slash");
+        }
+
+
+        public virtual void GuardThing()
+        {
+            Debug.Log("Guard thing");
+        }
+    }
+
+    public class SwordA : MeleeWeapon
+    {
+        public override int BaseAttackDamage { get; set; }
+
+        public override void DoAttack()
+        {
+            base.DoAttack();
+        }
+    }
+
+    public class Pistol : RangedWeapon
+    {
+        public override int BaseAttackDamage { get; set; }
+
+        public Vector3 gunPointTransform; // = to a hard coded constant because i am a lazy shit
+
+        public override void DoAttack()
+        {
+            base.DoAttack();
+        }
+
+    }
+    
+}
+
+
+
---
+++ Assets/WeaponController.cs
@@ -0,0 +1,33 @@
+﻿using Sirenix.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Sirenix.Serialization;
+using Sirenix.OdinInspector;
+
+namespace Weapon
+{
+    public class WeaponController : SerializedMonoBehaviour
+    {
+        [OdinSerialize] private IWeapon currentWeapon;
+
+        private void Awake()
+        {
+            //var currentWeaponComponents = currentWeapon.GetType().Assembly.GetTypes().
+            //Where(type => type.GetInterface(typeof(IWeaponComponent).Name) != null);
+
+            
+
+            //currentWeapon.Initialize();
+        }
+
+        private void InitializeTestWeapon()
+        {
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
